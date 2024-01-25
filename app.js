dd423c065c1acc4e2366d3235e1cc25259023aab
slugo01 <sergiocorreo24@gmail.com>
2024-01-25 15:53:22 +0000

    tarceclientev4

--- app.js
+++ app.js
@@ -49,3 +49,22 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        // Reenviar el mensaje a todos los demás clientes
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
+
+console.log('Servidor de señalización corriendo en el puerto 8080');
